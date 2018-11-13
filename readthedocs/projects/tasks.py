6050509dcefefaab61f4390211ca713a073af490
Manuel Kaufmann <humitos@gmail.com>
2018-11-13 11:03:42 +0100

    Notify users about the usage of deprecated webhooks
    Each time a deprecated webhook is hit, a notification is
    created (without duplicating it) to be sent.

--- readthedocs/projects/tasks.py
+++ readthedocs/projects/tasks.py
@@ -1367,3 +1367,8 @@ def finish_inactive_builds():
         'Builds marked as "Terminated due inactivity": %s',
         builds_finished,
     )
+
+
+@app.task
+def send_deprecated_endpoint_notifications():
+    pass
