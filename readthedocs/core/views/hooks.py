7d3e68e591a0af563fcfa429737375737e6a3c83
Manuel Kaufmann <humitos@gmail.com>
2018-11-19 19:30:27 +0100

    Improve decorator to support generic and specific VCS webhook views

--- readthedocs/core/views/hooks.py
+++ readthedocs/core/views/hooks.py
@@ -93,6 +93,7 @@ def log_info(project, msg):
                      msg=msg))
 
 
+@notify_deprecated_endpoint
 def _build_url(url, projects, branches):
     """
     Map a URL onto specific projects to build that are linked to that URL.
@@ -142,7 +143,6 @@ def _build_url(url, projects, branches):
 
 
 @csrf_exempt
-@notify_deprecated_endpoint
 def github_build(request):  # noqa: D205
     """
     GitHub webhook consumer.
@@ -198,7 +198,6 @@ def github_build(request):  # noqa: D205
 
 
 @csrf_exempt
-@notify_deprecated_endpoint
 def gitlab_build(request):  # noqa: D205
     """
     GitLab webhook consumer.
@@ -234,7 +233,6 @@ def gitlab_build(request):  # noqa: D205
 
 
 @csrf_exempt
-@notify_deprecated_endpoint
 def bitbucket_build(request):
     """
     Consume webhooks from multiple versions of Bitbucket's API.
